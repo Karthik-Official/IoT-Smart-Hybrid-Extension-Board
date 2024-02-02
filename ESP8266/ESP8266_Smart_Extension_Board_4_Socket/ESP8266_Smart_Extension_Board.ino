cff54241cad4417fd56411fd18f04a02aa35f514
Karthik-Official <karthikeyanofficial.edu@gmail.com>
2024-02-02 22:54:56 +0530

    Update-5 (Web Control)

--- ESP8266/ESP8266_Smart_Extension_Board_4_Socket/ESP8266_Smart_Extension_Board.ino
+++ ESP8266/ESP8266_Smart_Extension_Board_4_Socket/ESP8266_Smart_Extension_Board.ino
@@ -1,9 +1,6 @@
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 #include <ESP8266WiFi.h>
 
-#define TOGGLE_TRIGGER_PIN 1 // TX - pin will trigger the wifi configuration portal when set to LOW
-#define WIFI_LED_PIN 3 // RX - Wifi Connection Indicator
-
 #define PIN_RELAY_1 5  //D1
 #define PIN_RELAY_2 4  //D2
 #define PIN_RELAY_3 14 //D5
@@ -14,16 +11,24 @@
 #define TOUCH_SENSOR_3 13  //D7
 #define TOUCH_SENSOR_4 0   //RX
 
-int relayState1 = 0; // toggle state for relay 1 
-int relayState2 = 0; // toggle state for relay 2
-int relayState3 = 0; // toggle state for relay 3
-int relayState4 = 0; // toggle state for relay 4
+#define WIFI_LED_PIN 3 // RX - Wifi Connection Indicator
+#define TOGGLE_TRIGGER_PIN 1 // TX - pin will trigger the wifi configuration portal when set to LOW
 
 int timeout = 120; // wifi config trigger timeout
 
 WiFiServer server(80); // Set web server port number to 80
-
 String header; // Variable to store the HTTP request
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+int relayState1 = 0; // toggle state for relay 1 
+int relayState2 = 0; // toggle state for relay 2
+int relayState3 = 0; // toggle state for relay 3
+int relayState4 = 0; // toggle state for relay 4
 
 void wifiConnect() {
  
@@ -77,12 +82,12 @@ void relayOnOff(int relay, bool state) {
 
     switch(relay){
       case 1: 
-          if(state == true){
+          if(state == true && relayState1 == 1) {
             digitalWrite(PIN_RELAY_1, LOW); // turn on relay 1
             relayState1 = 0;
             Serial.println("Switch 1 ON");
           }
-          else{
+          else if (state == false && relayState1 == 0) {
             digitalWrite(PIN_RELAY_1, HIGH); // turn off relay 1
             relayState1 = 1;
             Serial.println("Switch 1 OFF");
@@ -90,12 +95,12 @@ void relayOnOff(int relay, bool state) {
           delay(100);
       break;
       case 2: 
-          if(state == true){
+          if(state == true  && relayState2 == 1) {
           digitalWrite(PIN_RELAY_2, LOW); // turn on relay 2
           relayState2 = 0;
           Serial.println("Switch 2 ON");
           }
-          else{
+          else if (state == false && relayState2 == 0) {
           digitalWrite(PIN_RELAY_2, HIGH); // turn off relay 2
           relayState2 = 1;
           Serial.println("Switch 2 OFF");
@@ -103,12 +108,12 @@ void relayOnOff(int relay, bool state) {
           delay(100);
       break;
       case 3: 
-          if(state == true){
+          if(state == true  && relayState3 == 1){
           digitalWrite(PIN_RELAY_3, LOW); // turn on relay 3
           relayState3 = 0;
           Serial.println("Switch 3 ON");
           }
-          else{
+          else if (state == false && relayState3 == 0) {
           digitalWrite(PIN_RELAY_3, HIGH); // turn off relay 3
           relayState3 = 1;
           Serial.println("Switch 3 OFF");
@@ -116,12 +121,12 @@ void relayOnOff(int relay, bool state) {
           delay(100);
       break;
       case 4: 
-          if(state == true){
+          if(state == true  && relayState4 == 1){
           digitalWrite(PIN_RELAY_4, LOW); // turn on relay 4
           relayState4 = 0;
           Serial.println("Switch 4 ON");
           }
-          else{
+          else if (state == false && relayState4 == 0) {
           digitalWrite(PIN_RELAY_4, HIGH); // turn off relay 4
           relayState4 = 1;
           Serial.println("Switch 4 OFF");
@@ -170,8 +175,148 @@ void touchControl() {
     
 }
 
-void webserverControl() { 
-    Serial.print("Hola Webserver");
+void webControl() { 
+
+    WiFiClient client = server.available();   // Listen for incoming clients
+
+    if (client) {                               // If a new client connects,
+     
+        Serial.println("New Client.");          // print a message out in the serial port
+        String currentLine = "";                // make a String to hold incoming data from the client
+        currentTime = millis();
+        previousTime = currentTime;
+        while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+            currentTime = millis();         
+            if (client.available()) {             // if there's bytes to read from the client,
+                char c = client.read();             // read a byte, then
+                Serial.write(c);                    // print it out the serial monitor
+                header += c;
+                if (c == '\n') {                    // if the byte is a newline character
+                    // if the current line is blank, you got two newline characters in a row.
+                    // that's the end of the client HTTP request, so send a response:
+                    if (currentLine.length() == 0) {
+                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                        // and a content-type so the client knows what's coming, then a blank line:
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println("Connection: close");
+                        client.println();
+                        
+                        // To turn the Relay on and off
+                        if (header.indexOf("GET /1/on") >= 0) {
+                            Serial.println("Relay 1 on");
+                            relayOnOff(1, true);
+                        } 
+                        else if (header.indexOf("GET /1/off") >= 0) {
+                            Serial.println("Relay 1 off");
+                            relayOnOff(1, false);
+                        } 
+
+                        if (header.indexOf("GET /2/on") >= 0) {
+                            Serial.println("Relay 2 on");
+                            relayOnOff(2, true);
+                        } 
+                        else if (header.indexOf("GET /2/off") >= 0) {
+                            Serial.println("Relay 2 off");
+                            relayOnOff(2, false);
+                        } 
+
+                        if (header.indexOf("GET /3/on") >= 0) {
+                            Serial.println("Relay 3 on");
+                            relayOnOff(3, true);
+                        } 
+                        else if (header.indexOf("GET /3/off") >= 0) {
+                            Serial.println("Relay 3 off");
+                            relayOnOff(3, false);
+                        } 
+
+                        if (header.indexOf("GET /4/on") >= 0) {
+                            Serial.println("Relay 4 on");
+                            relayOnOff(4, true);
+                        } 
+                        else if (header.indexOf("GET /4/off") >= 0) {
+                            Serial.println("Relay 4 off");
+                            relayOnOff(4, false);
+                        } 
+                                    
+                        // Display the HTML web page
+                        client.println("<!DOCTYPE html><html>");
+                        client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                        client.println("<link rel=\"icon\" href=\"data:,\">");
+                        // CSS to style the on/off buttons 
+                        // Feel free to change the background-color and font-size attributes to fit your preferences
+                        client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+                        client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+                        client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+                        client.println(".button2 {background-color: #77878A;}</style></head>");
+                        
+                        // Web Page Heading
+                        client.println("<body><h1>Smart Extension Board (ESP8266)</h1>");
+                        
+                        // // Display current state, and ON/OFF buttons for Relay 5  
+                        // client.println("<p>Relay 1 - State " + output5State + "</p>");
+
+                        // If the output5State is off, it displays the ON button       
+                        // if (output5State=="off") {
+                        //     client.print( " <p><button id='navigateButton1' class='button'>ON</button></a></p> <script> document.getElementById('navigateButton1').addEventListener('click', function() { window.location.href = '/5/on'; setTimeout(function() { window.location.href = '/';}, 1000);}); </script> ");   
+                        // } 
+                        // else {
+                        //     client.print( " <p><button id='navigateButton2' class='button button2'>OFF</button></a></p> <script> document.getElementById('navigateButton2').addEventListener('click', function() { window.location.href = '/5/off'; setTimeout(function() { window.location.href = '/';}, 1000);}); </script> ");             
+                        // } 
+                        
+                        // // Display current state, and ON/OFF buttons for Relay 4  
+                        // client.println("<p>Relay 4 - State " + output4State + "</p>");
+
+                        // If the output4State is off, it displays the ON button 
+
+                        if (relayState1==0) {
+                            client.println("<p><a href=\"/1/on\"><button class=\"button\">ON</button></a></p>");
+                        } 
+                        else {
+                            client.println("<p><a href=\"/1/off\"><button class=\"button button2\">OFF</button></a></p>");
+                        }
+                        if (relayState2==0) {
+                            client.println("<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>");
+                        } 
+                        else {
+                            client.println("<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>");
+                        }
+                        if (relayState3==0) {
+                            client.println("<p><a href=\"/3/on\"><button class=\"button\">ON</button></a></p>");
+                        } 
+                        else {
+                            client.println("<p><a href=\"/3/off\"><button class=\"button button2\">OFF</button></a></p>");
+                        }
+                        if (relayState4==0) {
+                            client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
+                        } 
+                        else {
+                            client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+                        }
+        
+                        client.println("</body></html>");
+                        // The HTTP response ends with another blank line
+                        client.println();
+                        // Break out of the while loop
+                        break;
+                    } 
+                    else { // if you got a newline, then clear currentLine
+                        currentLine = "";
+                    }
+
+                } 
+                else if (c != '\r') {  // if you got anything else but a carriage return character,
+                    currentLine += c;      // add it to the end of the currentLine
+                }
+            }
+        }
+        // Clear the header variable
+        header = "";
+        // Close the connection
+        client.stop();
+        Serial.println("Client disconnected.");
+        Serial.println("");
+    }    
 }
 
 void setup() {
@@ -204,7 +349,7 @@ void loop() {
 
     touchControl();
     if(WiFi.status() == WL_CONNECTED) {
-        webserverControl();
+        webControl();
     }
     
 }
