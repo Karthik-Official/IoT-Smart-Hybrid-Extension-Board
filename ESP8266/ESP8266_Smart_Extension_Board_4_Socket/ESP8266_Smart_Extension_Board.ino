e4de93ab8b92d1c10613fac2083cb17bf819071d
Karthik-Official <karthikeyanofficial.edu@gmail.com>
2024-02-02 20:42:45 +0530

    Update-4 (Touch Control)

--- ESP8266/ESP8266_Smart_Extension_Board_4_Socket/ESP8266_Smart_Extension_Board.ino
+++ ESP8266/ESP8266_Smart_Extension_Board_4_Socket/ESP8266_Smart_Extension_Board.ino
@@ -10,9 +10,9 @@
 #define PIN_RELAY_4 12 //D6
 
 #define TOUCH_SENSOR_1 10  //SD3
-#define TOUCH_SENSOR_2 0   //D3 
+#define TOUCH_SENSOR_2 9   //SD2 
 #define TOUCH_SENSOR_3 13  //D7
-#define TOUCH_SENSOR_4 9   //RX
+#define TOUCH_SENSOR_4 0   //RX
 
 int relayState1 = 0; // toggle state for relay 1 
 int relayState2 = 0; // toggle state for relay 2
@@ -35,13 +35,12 @@ void wifiConnect() {
         digitalWrite(WIFI_LED_PIN, HIGH); // Wifi Connection Mode Indicator On         
 
         WiFiManager wm;    
-        // wm.resetSettings(); // reset wifi settings - uncomment for testing
+        // wm.resetSettinxgs(); // reset wifi settings - uncomment for testing
         wm.setConfigPortalTimeout(timeout); // set configportal timeout
 
-        // uncomment below part-1 and part-2 for custom static IP address. CAUTION: Static IP may conflict IP DHCP IP address, 
-        // Only use if you know how to configure your router to exclude the static IP address from the DHCP scope and know your Gateway and Subnet mask.
+        // // uncomment below part-1 and part-2 for custom static IP address. CAUTION: Static IP may conflict IP DHCP IP address, 
+        // // Only use if you know how to configure your router to exclude the static IP address from the DHCP scope and know your Gateway and Subnet mask.
         // // Added parameters for static IP configuration part-1
-
         // WiFiManagerParameter custom_ip("ip", "Static IP", "192.168.1.100", 15); //Default IP address
         // WiFiManagerParameter custom_gateway("gateway", "Gateway", "192.168.1.1", 15); //Default Gateway
         // WiFiManagerParameter custom_subnet("subnet", "Subnet", "255.255.255.0", 15); //Default Subnet mask
@@ -53,32 +52,32 @@ void wifiConnect() {
         if (!wm.startConfigPortal("Smart-ExtBoard-AP")) {
             Serial.println("failed to connect and hit timeout");
             digitalWrite(WIFI_LED_PIN, LOW); // Wifi Connection Mode Indicator Off by Reset
-            ESP.restart(); //reset and try again
+            // ESP.reset(); //reset and try again
             delay(3000);
+        }
+        else {
+            //only prints when ESP connected to the WiFi
+            Serial.println("connected to the wifi)");
+            Serial.print("IP Address: ");
+            Serial.println(WiFi.localIP());
+            digitalWrite(WIFI_LED_PIN, LOW); // Wifi Connection Mode Indicator Off by Connection
         }
 
         // // Apply static IP configuration part-2
-
         // IPAddress staticIP, gateway, subnet;
         // staticIP.fromString(custom_ip.getValue());
         // gateway.fromString(custom_gateway.getValue());
         // subnet.fromString(custom_subnet.getValue());
         // WiFi.config(staticIP, gateway, subnet); 
-
-        digitalWrite(WIFI_LED_PIN, LOW); // Wifi Connection Mode Indicator Off by Connection
-
-        //only prints when ESP connected to the WiFi
-        Serial.println("connected to the wifi)");
-        Serial.print("IP Address: ");
-        Serial.println(WiFi.localIP());
     }
-}
 
-void relayOnOff(int relay) {
+} 
+
+void relayOnOff(int relay, bool state) {
 
     switch(relay){
       case 1: 
-          if(relayState1 == 1){
+          if(state == true){
             digitalWrite(PIN_RELAY_1, LOW); // turn on relay 1
             relayState1 = 0;
             Serial.println("Switch 1 ON");
@@ -91,7 +90,7 @@ void relayOnOff(int relay) {
           delay(100);
       break;
       case 2: 
-          if(relayState2 == 1){
+          if(state == true){
           digitalWrite(PIN_RELAY_2, LOW); // turn on relay 2
           relayState2 = 0;
           Serial.println("Switch 2 ON");
@@ -104,7 +103,7 @@ void relayOnOff(int relay) {
           delay(100);
       break;
       case 3: 
-          if(relayState3 == 1){
+          if(state == true){
           digitalWrite(PIN_RELAY_3, LOW); // turn on relay 3
           relayState3 = 0;
           Serial.println("Switch 3 ON");
@@ -117,7 +116,7 @@ void relayOnOff(int relay) {
           delay(100);
       break;
       case 4: 
-          if(relayState4 == 1){
+          if(state == true){
           digitalWrite(PIN_RELAY_4, LOW); // turn on relay 4
           relayState4 = 0;
           Serial.println("Switch 4 ON");
@@ -136,22 +135,39 @@ void relayOnOff(int relay) {
 void touchControl() {
 
     //Manual Touch Switch Control
-    if (digitalRead(TOUCH_SENSOR_1) == LOW){
-      delay(200);
-      relayOnOff(1);      
+    if (digitalRead(TOUCH_SENSOR_1) == HIGH){
+        delay(200);
+        relayOnOff(1, true);      
     }
-    else if (digitalRead(TOUCH_SENSOR_2) == LOW){
-      delay(200);
-      relayOnOff(2);
+    else if (digitalRead(TOUCH_SENSOR_1) == LOW) {
+        delay(200);
+        relayOnOff(1, false);  
     }
-    else if (digitalRead(TOUCH_SENSOR_3) == LOW){
-      delay(200);
-      relayOnOff(3);
+    if (digitalRead(TOUCH_SENSOR_2) == HIGH){
+        delay(200);
+        relayOnOff(2, true);      
     }
-    else if (digitalRead(TOUCH_SENSOR_4) == LOW){
-      delay(200);
-      relayOnOff(4);
+    else if (digitalRead(TOUCH_SENSOR_2) == LOW) {
+        delay(200);
+        relayOnOff(2, false);  
     }
+    if (digitalRead(TOUCH_SENSOR_3) == HIGH){
+        
+        relayOnOff(3, true);      
+    }
+    else if (digitalRead(TOUCH_SENSOR_3) == LOW) {
+        
+        relayOnOff(3, false);  
+    }
+    if (digitalRead(TOUCH_SENSOR_4) == HIGH){
+        delay(200);
+        relayOnOff(4, true);      
+    }
+    else if (digitalRead(TOUCH_SENSOR_4) == LOW) {
+        delay(200);
+        relayOnOff(4, false);  
+    }
+    
 }
 
 void webserverControl() { 
